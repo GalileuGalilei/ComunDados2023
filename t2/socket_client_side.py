ce58cef6041c4592a5d1766a2f44fdeb937aeda8
mrtrevisan <mauroroberto.trevisan@gmail.com>
2023-07-02 12:52:27 -0300

    Codigo macaco mas bem penteadinho

--- t2/socket_client_side.py
+++ t2/socket_client_side.py
@@ -1,55 +1,61 @@
-#faça um socket server side
-#faça um socket client side
-#faça um socket server side que receba uma mensagem e retorne a mesma mensagem em maiúsculo
-#faça um socket client side que envie uma mensagem e receba a mesma mensagem em maiúsculo
-
 # Import socket module
-import socket            
+import socket                    
  
-# Create a socket object
-s = socket.socket()        
- 
-# Define the port on which you want to connect            
-
 def get_data_from_frame(frame):
+    #monta a flag referencia
     flag = ['0', '1', '1', '1', '1', '1', '1', '0']
+
+    #pega o header, corpo da msg, trailer e flag da msg
     header = frame[8:16]
     data = frame[16:-16]
     trailer = frame[-16:-8]
     flag2 = frame[-8:]
 
+    #compara as flags
     if flag != flag2:
         print("Error: flag != flag2")
         return None
-
+    
+    #verifica o trailer
     if trailer != ['0'] * 8:
         print("Error: trailer != ['0'] * 8")
         return None
 
+    #retorna a mensagem
     size = int(''.join(header), 2)
     return data[:size]
 
+#converte lista de bits pra string
 def bit_list_to_string(bit_list):
     byte_list = ["".join(bit_list[i:i+8]) for i in range(0, len(bit_list), 8)]  # Group bits into bytes
     char_list = [chr(int(byte, 2)) for byte in byte_list]  # Convert bytes to characters
     return "".join(char_list)
 
+#remove o bit stuffing
 def remove_bit_stuffing(data):
     for i in range(len(data) - 7):
         if data[i : i+7] == ['0', '1', '1', '1', '1', '1', '0']:
             data.pop(i+6)
 
-# connect to the server on local computer
-port = 12345   
-s.connect(('127.0.0.1', port))
+def main():
+    # Create a socket object
+    s = socket.socket()
 
+    # Define the port on which you want to connect            
+    port = 12345   
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', port))
 
-# receive data from the server and decoding to get the string.
-raw_data = s.recv(10024)
-data = get_data_from_frame(list(raw_data.decode()))
-remove_bit_stuffing(data)
-print(data)
-print(bit_list_to_string(data))
-# close the connection
-s.close() 
+    # receive data from the server and decoding to get the string.
+    raw_data = s.recv(1024)
+    data = get_data_from_frame(list(raw_data.decode()))
+    #remove o bit extra do bit stuffing
+    remove_bit_stuffing(data)
+    
+    #printa a mensagem decodificada
+    print(bit_list_to_string(data))
+    # close the connection
+    s.close() 
 
+if __name__ == '__main__':
+    main()
--- t2/socket_server_side.py
+++ t2/socket_server_side.py
@@ -1,37 +1,12 @@
 # first of all import the socket library
-import socket	
+import socket		
 
-# next create a socket object
-s = socket.socket()		
-print ("Socket successfully created")
-
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12345			
-
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))		
-print ("socket binded to %s" %(port))
-
-# put the socket into listening mode
-s.listen(5)	
-print ("socket is listening")		
-
-#frame 
-#flag = 01111110
-def data_bit_stuffing(data):
-    for i in range(len(data) - 6):
-        if data[i : i+6] == ['0', '1', '1', '1', '1', '1']:
-            data.insert(i+6, '0')
-
+#converte um int para uma lista de bits
 def integer_to_bit_list(integer):
     bits = bin(integer)[2:].zfill(8)  # Convert the ASCII value to a binary string
     return list(bits)
 
+#converte uma string para uma lista de bits
 def string_to_bit_list(string):
     bit_list = []
     for char in string:
@@ -40,15 +15,29 @@ def string_to_bit_list(string):
         bit_list.extend(list(bits))  # Add each bit to the bit list
     return bit_list
 
-def make_frame(data):
-    data_bit_stuffing(data)
+#frame 
+#flag = 01111110
+def data_bit_stuffing(data):
+    for i in range(len(data) - 6):
+        if data[i : i+6] == ['0', '1', '1', '1', '1', '1']:
+            data.insert(i+6, '0')
 
+#monta o frame
+def make_frame(data):
+    #monta a flag
+    flag = ['0', '1', '1', '1', '1', '1', '1', '0']
+
+    #monta o header
     size = len(data)
     header = integer_to_bit_list(size)
-    flag = ['0', '1', '1', '1', '1', '1', '1', '0']
-    #por enquanto, 8 '0's
+
+    #faz bit stuffing da mensagem
+    data_bit_stuffing(data)
+
+    #monta o trailer, por enquanto, 8 '0's
     trailer = ['0'] * 8
 
+    #monta o frame
     bitarray = []
     bitarray += flag
     bitarray += header
@@ -58,33 +47,52 @@ def make_frame(data):
 
     return bitarray
 
-def char_list_to_bytes(char_list):
-    return bytes(char_list)
+def main():
+    # next create a socket object
+    s = socket.socket()		
+    print ("Socket successfully created")
 
-# a forever loop until we interrupt it or
-# an error occurs
-data = 'Chupa meu pau'
-data = string_to_bit_list(data)
-print(data)
-data = make_frame(data)
-str = ''
+    # reserve a port on your computer in our
+    # case it is 12345 but it can be anything
+    port = 12345			
 
-for i in range(len(data)):
-    str += data[i]
+    # Next bind to the port
+    # we have not typed any ip in the ip field
+    # instead we have inputted an empty string
+    # this makes the server listen to requests
+    # coming from other computers on the network
+    s.bind(('', port))		
+    print ("socket binded to %s" %(port))
 
+    # put the socket into listening mode
+    s.listen(5)	
+    print ("socket is listening")   
+    	
+    # define a mensagem a ser enviada
+    data = 'Chupa meu pau'
+    #transforma em uma lista de bits
+    data = string_to_bit_list(data)
+    #monta o frame
+    data = make_frame(data)
+    #monta novamente em uma string
+    str = ''
+    for i in range(len(data)):
+        str += data[i]
 
-while True:
+    # a forever loop until we interrupt it or
+    # an error occurs
+    while True:
+        # wait for.
+        c, addr = s.accept()	
+        print ('Got connection from', addr )
 
-    # wait for.
-    c, addr = s.accept()	
-    print ('Got connection from', addr )
+        # send a thank you message to the client. encoding to send byte type.
+        c.send(str.encode())
 
-    # send a thank you message to the client. encoding to send byte type.
-    
-    c.send(str.encode())
+        # Close the connection with the client
+        c.close()
+        # Breaking once connection closed
+        break
 
-    # Close the connection with the client
-    c.close()
-
-    # Breaking once connection closed
-    break
+if __name__ == '__main__':
+    main()
